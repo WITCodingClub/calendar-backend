1a5b524a1eceb111f91ad553df4497b7aadc96fe
Jasper Mayone <mayonej@wit.edu>
2025-10-30 15:51:50 -0400

    re-annotate models

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -3,10 +3,19 @@
 # Table name: rooms
 # Database name: primary
 #
-#  id         :bigint           not null, primary key
-#  number     :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  number      :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  building_id :bigint           not null
+#
+# Indexes
+#
+#  index_rooms_on_building_id  (building_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (building_id => buildings.id)
 #
 FactoryBot.define do
   factory :room do
