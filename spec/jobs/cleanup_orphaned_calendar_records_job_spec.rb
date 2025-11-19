6ef255b96f522fbe3666a387d35617e22e50c9c0
Jasper Mayone <mayonej@wit.edu>
2025-11-19 11:31:18 -0500

    jobs cleanup

--- spec/jobs/cleanup_orphaned_calendar_records_job_spec.rb
+++ spec/jobs/cleanup_orphaned_calendar_records_job_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe DeleteOrphanedGoogleCalendarsJob do
+RSpec.describe CleanupOrphanedCalendarRecordsJob do
   describe "queue assignment" do
     it "is assigned to the low queue" do
       expect(described_class.new.queue_name).to eq("low")
