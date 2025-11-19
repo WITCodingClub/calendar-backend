5cd2840f2d35d1b1fa7920b954e2bc0d61ab31cc
Jasper Mayone <mayonej@wit.edu>
2025-11-19 17:27:44 -0500

    new flags (#163)

--- app/lib/flipper_flags.rb
+++ app/lib/flipper_flags.rb
@@ -3,15 +3,23 @@
 module FlipperFlags
   V1 = :"2025_10_04_v1"
   V2 = :"2025_11_12_v2"
+  # rubocop:disable Lint/SymbolConversion, Style/SymbolLiteral
+  ENV_SWITCHER = :"env_switcher"
+  DEBUG_MODE = :"debug_mode"
+  # rubocop:enable Lint/SymbolConversion, Style/SymbolLiteral
 
   MAP = {
     v1: V1,
-    v2: V2
+    v2: V2,
+    envSwitcher: ENV_SWITCHER,
+    debugMode: DEBUG_MODE,
   }.freeze
 
   ALL_FLAGS = [
     :v1,
     :v2,
+    :envSwitcher,
+    :debugMode,
   ].freeze
 
 
