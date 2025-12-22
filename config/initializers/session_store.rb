c12a394cf711884acdc22f2b120501b6b563e070
Jasper Mayone <mayonej@wit.edu>
2025-12-22 17:15:39 -0500

    extend session time

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.session_store :redis_session_store,
                                        servers: ENV.fetch("REDIS_URL", "redis://localhost:6379/2/session"),
-                                       expire_after: Rails.env.development? ? 30.days : 90.minutes,
+                                       expire_after: 30.days,
                                        key: "_#{Rails.application.class.module_parent_name.underscore}_session",
                                        threadsafe: true,
                                        signed: true,
