5c018672c64b94b049429e64c8de24f38992c5de
Jasper Mayone <mayonej@wit.edu>
2026-01-06 18:16:58 -0500

    hotfix: update migration

--- db/migrate/20260106230648_fix_courses_crn_uniqueness.rb
+++ db/migrate/20260106230648_fix_courses_crn_uniqueness.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class FixCoursesCrnUniqueness < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
+
   def change
     # Remove the unique constraint on CRN alone
-    remove_index :courses, :crn
+    remove_index :courses, :crn, algorithm: :concurrently
     
     # Add a compound unique index on CRN + term_id
-    add_index :courses, [:crn, :term_id], unique: true, name: 'index_courses_on_crn_and_term_id'
+    add_index :courses, [:crn, :term_id], unique: true, name: 'index_courses_on_crn_and_term_id', algorithm: :concurrently
     
     # Keep a non-unique index on CRN for performance
-    add_index :courses, :crn
+    add_index :courses, :crn, algorithm: :concurrently
   end
 
 end
