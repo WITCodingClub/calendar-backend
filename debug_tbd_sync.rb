e7ed946cdaf337fc0278dc5285996e61a28b809d
Jasper Mayone <mayonej@wit.edu>
2026-01-07 10:24:59 -0500

    debug: fix syntax errors in TBD debug script

--- debug_tbd_sync.rb
+++ debug_tbd_sync.rb
@@ -34,45 +34,44 @@ if enrolled_tbd_courses.any?
     enrollment = Enrollment.find_by(user: user, course: course)
     puts "User enrolled: #{!!enrollment}"
 
-      if enrollment
-        puts "Day of week: #{mt.day_of_week}"
+    if enrollment && mt.day_of_week.present?
+      puts "Day of week: #{mt.day_of_week}"
+      
+      first_meeting_date = user.send(:find_first_meeting_date, mt)
+      puts "First meeting date: #{first_meeting_date}"
+      
+      if first_meeting_date
+        start_time = user.send(:parse_time, first_meeting_date, mt.begin_time)
+        end_time = user.send(:parse_time, first_meeting_date, mt.end_time)
+        puts "Times: #{start_time} - #{end_time}"
         
-        if mt.day_of_week.present?
-          first_meeting_date = user.send(:find_first_meeting_date, mt)
-          puts "First meeting date: #{first_meeting_date}"
+        if start_time && end_time
+          puts "\n--- TBD CHECKS ---"
           
-          if first_meeting_date
-            start_time = user.send(:parse_time, first_meeting_date, mt.begin_time)
-            end_time = user.send(:parse_time, first_meeting_date, mt.end_time)
-            puts "Times: #{start_time} - #{end_time}"
+          building_tbd = mt.building && user.send(:tbd_building?, mt.building)
+          room_tbd = mt.room && user.send(:tbd_room?, mt.room) 
+          location_tbd = mt.room && mt.building && user.send(:tbd_location?, mt.building, mt.room)
+          
+          puts "Building TBD check: #{building_tbd}"
+          puts "Room TBD check: #{room_tbd}"
+          puts "Location TBD check: #{location_tbd}"
+          
+          should_skip = building_tbd || room_tbd || location_tbd
+          puts "SHOULD SKIP: #{should_skip}"
+          
+          if should_skip
+            puts "✅ This meeting time should be SKIPPED"
+          else
+            puts "❌ This meeting time would CREATE AN EVENT"
             
-            if start_time && end_time
-              puts "\n--- TBD CHECKS ---"
-              
-              building_tbd = mt.building && user.send(:tbd_building?, mt.building)
-              room_tbd = mt.room && user.send(:tbd_room?, mt.room) 
-              location_tbd = mt.room && mt.building && user.send(:tbd_location?, mt.building, mt.room)
-              
-              puts "Building TBD check: #{building_tbd}"
-              puts "Room TBD check: #{room_tbd}"
-              puts "Location TBD check: #{location_tbd}"
-              
-              should_skip = building_tbd || room_tbd || location_tbd
-              puts "SHOULD SKIP: #{should_skip}"
-              
-              if should_skip
-                puts "✅ This meeting time should be SKIPPED"
-              else
-                puts "❌ This meeting time would CREATE AN EVENT"
-                
-                location = if mt.room && mt.building
-                             "#{mt.building.name} - #{mt.room.formatted_number}"
-                           elsif mt.room
-                             mt.room.formatted_number
-                           end
-                puts "Location that would be created: '#{location}'"
-              end
-            end
+            location = if mt.room && mt.building
+                         "#{mt.building.name} - #{mt.room.formatted_number}"
+                       elsif mt.room
+                         mt.room.formatted_number
+                       else
+                         nil
+                       end
+            puts "Location that would be created: '#{location}'"
           end
         end
       end
