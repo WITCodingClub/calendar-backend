cc78178910e9c102ecd22f62841cee4df41f09c4
Jasper Mayone <mayonej@wit.edu>
2026-01-12 09:35:28 -0500

    Add initializer to load encryption keys from env vars

---
+++ config/initializers/active_record_encryption.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Configure Active Record encryption from environment variables (for CI)
+# when credentials are not available
+if ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"].present?
+  Rails.application.config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+  Rails.application.config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+  Rails.application.config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+end
