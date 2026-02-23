4593a5be7ab1e44dca298a82e888d14d3260e4e5
Jasper Mayone <mayonej@wit.edu>
2026-02-22 22:29:43 -0500

    fix: remove PublicIdentifiable from Friendship (hashid-rails was removed)
    Commit assisted by Claude

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -26,10 +26,6 @@
 #  fk_rails_...  (requester_id => users.id)
 #
 class Friendship < ApplicationRecord
-  include PublicIdentifiable
-
-  set_public_id_prefix :frn, min_hash_length: 10
-
   belongs_to :requester, class_name: "User"
   belongs_to :addressee, class_name: "User"
 
