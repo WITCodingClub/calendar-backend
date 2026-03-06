fc88c31608d679b3902b74df7ba410c6e6639155
Daniel Miretsky <miretskyd@wit.edu>
2026-03-06 16:53:03 -0500

    move from current and next term to all active terms

--- app/controllers/api/misc_controller.rb
+++ app/controllers/api/misc_controller.rb
@@ -2,16 +2,14 @@
 
 module Api
   class MiscController < ApiController
-    skip_before_action :authenticate_user_from_token!, only: [:get_current_terms]
-    skip_before_action :check_beta_access, only: [:get_current_terms]
+    skip_before_action :authenticate_user_from_token!, only: [:get_active_terms]
+    skip_before_action :check_beta_access, only: [:get_active_terms]
 
-    def get_current_terms
-      current_term = Term.current
-      next_term = Term.next
+    def get_active_terms
+      active_terms = Term.active
 
       render json: {
-        current_term: TermSerializer.new(current_term).as_json,
-        next_term: TermSerializer.new(next_term).as_json
+        active_terms: active_terms.map { |term| TermSerializer.new(term).as_json }
       }, status: :ok
     end
 
