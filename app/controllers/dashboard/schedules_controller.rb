2b18bc22c0d5d04a73014d15fd2a213ea8f6c311
Jasper Mayone <mayonej@wit.edu>
2026-06-13 17:59:20 -0400

    CATCH

--- app/controllers/dashboard/schedules_controller.rb
+++ app/controllers/dashboard/schedules_controller.rb
@@ -13,6 +13,29 @@ class Dashboard::SchedulesController < Dashboard::ApplicationController
                        Term.current || @terms.first
                      end
 
+    @view_mode = params[:view].presence_in(%w[list week month]) || "week"
+    @today     = Time.zone.today
+
+    @week_start = if params[:week_start].present?
+                    begin
+                      Date.parse(params[:week_start]).beginning_of_week(:monday)
+                    rescue ArgumentError, TypeError
+                      @today.beginning_of_week(:monday)
+                    end
+                  else
+                    @today.beginning_of_week(:monday)
+                  end
+
+    @month_date = if params[:month].present?
+                    begin
+                      Date.parse("#{params[:month]}-01").beginning_of_month
+                    rescue ArgumentError, TypeError
+                      @today.beginning_of_month
+                    end
+                  else
+                    @today.beginning_of_month
+                  end
+
     return unless @selected_term
 
     enrollments = current_user
@@ -34,5 +57,25 @@ class Dashboard::SchedulesController < Dashboard::ApplicationController
         template_renderer:   template_renderer
       ).as_json.with_indifferent_access
     end
+
+    @schedule_by_day = Hash.new { |h, k| h[k] = [] }
+    @courses.each do |course|
+      (course[:meeting_times] || []).each do |mt|
+        %w[monday tuesday wednesday thursday friday saturday sunday].each do |day|
+          @schedule_by_day[day] << { course: course, meeting_time: mt } if mt[day]
+        end
+      end
+    end
+
+    @no_class_dates = []
+    UniversityCalendarEvent
+      .where(category: %w[holiday study_day finals])
+      .where(term_id: [@selected_term.id, nil])
+      .find_each do |event|
+        s = event.start_time.to_date
+        e = event.end_time&.to_date || s
+        (s..e).each { |d| @no_class_dates << d }
+      end
+    @no_class_dates.uniq!
   end
 end
