e1b4bcf38a989b2c249efe5872ba93ddbe76ef24
Cattn <Logan@cattn.dev>
2026-06-18 15:36:29 -0400

    remove redirect
    Closes #474

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@
 
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to after_sign_in_path_for(current_user)
-    else
       render plain: "Not Found", status: :not_found
-    end
   end
 end
