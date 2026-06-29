4e666bd0efb02d8d6ea7f4702a870938b518b924
Daniel Miretsky <miretskyd@wit.edu>
2026-06-28 20:42:36 -0400

    fix issue in home_controller.rb
    with the original code, when trying to run the dev server from inside a codespace I am getting a "Not Found" screen even though everything is set correctly. This fixed the issue for me. Not sure if this is the right fix but it worked for me at least.
    
    Signed-off-by: Daniel Miretsky <miretskyd@wit.edu>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@
 
 class HomeController < ApplicationController
   def index
-      render plain: "Not Found", status: :not_found
+    if user_signed_in?
+      redirect_to dashboard_root_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
